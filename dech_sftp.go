fca050040645efde210980dcef154b81f02c3288
Ruangdech  Kurunat <ruangdech@gmail.com>
2023-03-23 12:46:04 +0700

    update show msg

--- dech_sftp.go
+++ dech_sftp.go
@@ -189,40 +189,60 @@ func ChangeModeDirOrFile(client *sftp.Client, remoteDirOrFile string, fileMode f
 }
 
 // Download file from sftp server
-func DownloadFile(client *sftp.Client, remoteFile, localFile string) (int64, error) {
-	fmt.Fprintf(os.Stdout, "Downloading [%s] to [%s] ...\n", remoteFile, localFile)
+func DownloadFile(client *sftp.Client, remoteFile, localFile string, isShowMsg bool) (int64, error) {
+	if isShowMsg {
+		fmt.Fprintf(os.Stdout, "Downloading [%s] to [%s] ...\n", remoteFile, localFile)
+	}
+
 	// Note: SFTP To Go doesn't support O_RDWR mode
 	srcFile, err := client.OpenFile(remoteFile, (os.O_RDONLY))
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to open remote file: %v\n", err)
+		if isShowMsg {
+			fmt.Fprintf(os.Stderr, "Unable to open remote file: %v\n", err)
+		}
+
 		return -1, err
 	}
 	defer srcFile.Close()
 
 	dstFile, err := os.Create(localFile)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to open local file: %v\n", err)
+		if isShowMsg {
+			fmt.Fprintf(os.Stderr, "Unable to open local file: %v\n", err)
+		}
+
 		return -1, err
 	}
 	defer dstFile.Close()
 
 	bytes, err := io.Copy(dstFile, srcFile)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to download remote file: %v\n", err)
+		if isShowMsg {
+			fmt.Fprintf(os.Stderr, "Unable to download remote file: %v\n", err)
+		}
+
 		os.Exit(1)
 	}
-	fmt.Fprintf(os.Stdout, "%d bytes copied\n", bytes)
+
+	if isShowMsg {
+		fmt.Fprintf(os.Stdout, "%d bytes copied\n", bytes)
+	}
 
 	return bytes, err
 }
 
 // Upload file to sftp server
-func UploadFile(client *sftp.Client, localFile, remoteFile string) (int64, error) {
-	fmt.Fprintf(os.Stdout, "Uploading [%s] to [%s] ...\n", localFile, remoteFile)
+func UploadFile(client *sftp.Client, localFile, remoteFile string, isShowMsg bool) (int64, error) {
+	if isShowMsg {
+		fmt.Fprintf(os.Stdout, "Uploading [%s] to [%s] ...\n", localFile, remoteFile)
+	}
 
 	srcFile, err := os.Open(localFile)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to open local file: %v\n", err)
+		if isShowMsg {
+			fmt.Fprintf(os.Stderr, "Unable to open local file: %v\n", err)
+		}
+
 		return -1, err
 	}
 	defer srcFile.Close()
@@ -230,17 +250,26 @@ func UploadFile(client *sftp.Client, localFile, remoteFile string) (int64, error
 	// Note: SFTP To Go doesn't support O_RDWR mode
 	dstFile, err := client.OpenFile(remoteFile, (os.O_WRONLY | os.O_CREATE | os.O_TRUNC))
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to open remote file: %v\n", err)
+		if isShowMsg {
+			fmt.Fprintf(os.Stderr, "Unable to open remote file: %v\n", err)
+		}
+
 		return -1, err
 	}
 	defer dstFile.Close()
 
 	bytes, err := io.Copy(dstFile, srcFile)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to upload local file: %v\n", err)
+		if isShowMsg {
+			fmt.Fprintf(os.Stderr, "Unable to upload local file: %v\n", err)
+		}
+
 		os.Exit(1)
 	}
-	fmt.Fprintf(os.Stdout, "%d bytes copied\n", bytes)
+
+	if isShowMsg {
+		fmt.Fprintf(os.Stdout, "%d bytes copied\n", bytes)
+	}
 
 	return bytes, err
 }
